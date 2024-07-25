b5ec51a40ddb89e5beabca9a09f1aa335e1978c0
Manojkumar <mk2961628@gmail.com>
2024-07-25 07:26:02 +0530

    published

--- src/app/carousal2/carousal2.component.ts
+++ src/app/carousal2/carousal2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-carousal2',
@@ -6,33 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './carousal2.component.css'
 })
 export class Carousal2Component {
-   currentIndex = 0;
-   slidesToShow = 3; 
-  totalSlides = 6; 
-
-  prevSlide() {
-    const slides = document.querySelector('.slider') as HTMLElement;
-    const slideWidth = slides.children[0].clientWidth;
-    
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    } else {
-      this.currentIndex = this.totalSlides - this.slidesToShow; 
-    }
-
-    slides.style.transform = `translateX(-${this.currentIndex * slideWidth}px)`;
-  }
-
-  nextSlide() {
-    const slides = document.querySelector('.slider') as HTMLElement;
-    const slideWidth = slides.children[0].clientWidth;
-
-    if (this.currentIndex < this.totalSlides - this.slidesToShow) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0; 
-    }
-
-    slides.style.transform = `translateX(-${this.currentIndex * slideWidth}px)`;
-  }
+  
 }
